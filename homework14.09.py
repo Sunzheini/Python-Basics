ef55bb1ac7064b95bb09308386fa4a6c8c6a45c8
Sunzheini <daniel_zorov@abv.bg>
2022-04-21 17:22:58 +0300

    first commit

---
+++ homework14.09.py
@@ -0,0 +1,21 @@
+
+# 1.1
+print('Hello SoftUni')
+
+# 1.2
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+print(6)
+print(7)
+print(8)
+print(9)
+print(10)
+
+# 1.3
+a = int(input())
+b = int(input())
+area = a * b
+print(area)
---
+++ lab1209.py
@@ -0,0 +1,20 @@
+# zadacha 1
+usd = float(input())
+course_usd_to_bgn = 1.79549
+bgn = usd * course_usd_to_bgn
+print(bgn)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
