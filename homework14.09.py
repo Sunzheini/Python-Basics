a4ff79f27f58aea964ad49f4df308a04ac5842ae
Sunzheini <daniel_zorov@abv.bg>
2022-04-22 16:53:43 +0300

    second commit

--- homework14.09.py
+++ homework14.09.py
@@ -1,21 +1,26 @@
 
 # 1.1
-print('Hello SoftUni')
+# print('Hello SoftUni')
 
 # 1.2
-print(1)
-print(2)
-print(3)
-print(4)
-print(5)
-print(6)
-print(7)
-print(8)
-print(9)
-print(10)
+# print(1)
+# print(2)
+# print(3)
+# print(4)
+# print(5)
+# print(6)
+# print(7)
+# print(8)
+# print(9)
+# print(10)
 
 # 1.3
-a = int(input())
-b = int(input())
-area = a * b
-print(area)
+# a = int(input())
+# b = int(input())
+# area = a * b
+# print(area)
+
+# 1.4
+inch = float(input())
+result = inch * 2.54
+print(result)
--- lab1209.py
+++ lab1209.py
@@ -1,10 +1,17 @@
+import math
+
 # zadacha 1
-usd = float(input())
-course_usd_to_bgn = 1.79549
-bgn = usd * course_usd_to_bgn
-print(bgn)
+# usd = float(input())
+# course_usd_to_bgn = 1.79549
+# bgn = usd * course_usd_to_bgn
+# print(bgn)
 
-
+# # zadacha 2
+#  floor zakryglqva kym dolnoto nai blizko chislo
+radians = float(input())
+# print(math.floor(radians))
+degrees = radians * 180 / math.pi
+print(degrees)
 
 
 
