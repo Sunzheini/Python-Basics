66bf94f06a1b8c62dc5c5e7866669186562d6538
Sunzheini <daniel_zorov@abv.bg>
2022-05-01 09:36:38 +0300

    commit010522-2

--- homework14.09.py
+++ homework14.09.py
@@ -24,3 +24,133 @@
 # inch = float(input())
 # result = inch * 2.54
 # print(result)
+
+# 1.5
+# name = input()
+# print("Hello, " + name + "!")
+
+# 1.6
+# first_name = input()
+# last_name = input()
+# age = int(input())
+# town = input()
+# print(f'You are {first_name} {last_name}, a {age}-years old person from {town}.')
+
+# 1.7
+# name = input()
+# number_of_projects = int(input())
+# time_to_complete_all_projects = number_of_projects * 3
+# print(f"The architect {name} will need {time_to_complete_all_projects} "
+#       f"hours to complete {number_of_projects} project/s.")
+
+# 1.8
+# number_dog = int(input())
+# number_cat = int(input())
+# total = number_dog * 2.50 + number_cat * 4
+# print(f"{total} lv.")
+
+# 1.9
+# square_meters_for_landscaping = float(input())
+# price_for_the_whole_yard = square_meters_for_landscaping * 7.61
+# discount = price_for_the_whole_yard * 0.18
+# total_sum = price_for_the_whole_yard - discount
+# print(f"The final price is: {total_sum} lv.")
+# print(f"The discount is: {discount} lv.")
+
+#
+# 2.1
+# usd = float(input())
+# bgn = usd * 1.79549
+# print(bgn)
+
+# 2.2
+# from math import pi
+# radians = float(input())
+# degrees = radians * 180 / pi
+# print(degrees)
+
+# 2.3
+# deposit = float(input())
+# months = int(input())
+# annual_interest_percent = float(input())
+# annual_interest = deposit * annual_interest_percent / 100
+# monthly_interest = annual_interest / 12
+# total_sum = deposit + (months * monthly_interest)
+# print(total_sum)
+
+# 2.4
+# from math import floor
+# number_of_pages = int(input())
+# pages_per_hour = int(input())
+# number_of_days = int(input())
+# total_time = number_of_pages / pages_per_hour
+# needed_time_per_day = total_time / number_of_days
+# print(floor(needed_time_per_day))
+
+# 2.5
+# price_of_pens = 5.80
+# price_of_markers = 7.20
+# price_of_detergent = 1.20
+#
+# number_of_pens = int(input())
+# number_of_markers = int(input())
+# liters_of_detergent = int(input())
+# discount = int(input())
+#
+# before_discount = number_of_pens * price_of_pens +\
+#                   number_of_markers * price_of_markers +\
+#                   liters_of_detergent * price_of_detergent
+# discount = before_discount / 100 * discount
+# total = before_discount - discount
+# print(total)
+
+# 2.6
+# price_of_nylon_per_meter = 1.50
+# price_of_paint_per_liter = 14.50
+# price_of_prep_per_liter = 5.00
+#
+# needed_quantity_of_nylon = int(input())
+# needed_quantity_of_paint = int(input())
+# needed_quantity_of_prep = int(input())
+# hours = int(input())
+#
+# materials = (needed_quantity_of_nylon + 2) * price_of_nylon_per_meter +\
+#             needed_quantity_of_paint * 1.1 * price_of_paint_per_liter +\
+#             needed_quantity_of_prep * price_of_prep_per_liter + 0.40
+# hour_rate = materials * 0.3
+# total = materials + hours * hour_rate
+#
+# print(total)
+
+# 2.7
+# chicken = 10.35
+# fish = 12.40
+# vege = 8.15
+# number_chicken = int(input())
+# number_fish = int(input())
+# number_vege = int(input())
+# menus = number_chicken * chicken + number_fish * fish + number_vege * vege
+# desert = menus * 0.2
+# total = menus + desert + 2.50
+# print(total)
+
+# 2.8
+# tax = int(input())
+# sneakers = tax * 0.6
+# clothes = sneakers * 0.8
+# ball = clothes * 0.25
+# accessories = ball * 0.2
+# total = tax + sneakers + clothes + ball + accessories
+# print(total)
+
+# 2.9
+# length_in_cm = int(input())
+# width_in_cm = int(input())
+# height_in_cm = int(input())
+# percent = float(input())
+# volume = length_in_cm * width_in_cm * height_in_cm
+# liters = volume / 1000
+# final = liters - liters / 100 * percent
+# print(final)
+
+
--- lab1209.py
+++ lab1209.py
@@ -1,4 +1,6 @@
 import math
+# from math import floor -
+# samo 1 neshto importirame i sled tova ne pishem math predi .floor
 
 # zadacha 1
 # usd = float(input())
@@ -7,11 +9,51 @@ import math
 # print(bgn)
 
 # # zadacha 2
-#  floor zakryglqva kym dolnoto nai blizko chislo
+# #  floor zakryglqva kym dolnoto nai blizko chislo
 # radians = float(input())
-# print(math.floor(radians))
+# # print(math.floor(radians))
 # degrees = radians * 180 / math.pi
 # print(degrees)
+
+# zadacha 3
+# deposit = float(input())
+# months = int(input())
+# annual_interest_percent = float(input())
+# annual_interest = deposit * annual_interest_percent / 100
+# monthly_interest = annual_interest / 12
+# total_sum = deposit + (months * monthly_interest)
+# print(total_sum)
+
+# zadacha 4
+
+# zadacha 5
+# number_of_pens = int(input())
+# number_of_markers = int(input())
+# liters_of_detergent = int(input())
+# percent_discount = int(input())
+#
+# price_for_pen = 5.80
+# price_for_marker = 7.20
+# price_for_liter_detergent = 1.20
+#
+# needed_sum = number_of_pens * price_for_pen \
+#              + number_of_markers * price_for_marker \
+#              + liters_of_detergent * price_for_liter_detergent
+# percent_discount /= 100
+# needed_sum *= (100 - percent_discount) / 100
+# print(needed_sum)
+
+# zadacha 8
+# annual_tax = int(input())
+# sneakers_price = annual_tax - annual_tax * 0.4
+# dress_price = sneakers_price - sneakers_price * 0.2
+# ball_price = dress_price / 4
+# accessories = ball_price / 5
+# total_sum = annual_tax + sneakers_price + \
+#             dress_price + ball_price + accessories
+# print(total_sum)
+
+
 
 
 
